54d1ee75c9fd1d6f9222b5ece8f535aecc793cb0
Alieksiei martins <alieksieimartins@alieksiei-martinss-macbook-10.local>
2018-03-12 16:16:50 -0300

    category database implement. add a second tableview controller.

--- ToDoEy2/Controllers/CategoriesViewController.swift
+++ ToDoEy2/Controllers/CategoriesViewController.swift
@@ -7,13 +7,82 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoriesViewController: UITableViewController {
+    
+    var categoryArray = [Category]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Categories.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(dataFilePath)
+        loadCategories()
 
     }
 
-  
+    // MARK: categories datasources methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categoryArray[indexPath.row].name
+        return cell
+    }
+    
+    
+    @IBAction func newCategoryButPresesd(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController (title: "Add a New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text
+            self.categoryArray.append(newCategory)
+            self.saveCategory()
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (newText) in
+            newText.placeholder = "Create Nw Category"
+            textField = newText
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: - model manipulation methods
+    
+    func saveCategory() {
+    
+    do {
+    try context.save()
+    } catch {
+    }
+    
+    tableView.reloadData()
+        
+    }
+    
+    func loadCategories (with request:NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            
+        }
+        tableView.reloadData()
+        
+    }
+    
 }
